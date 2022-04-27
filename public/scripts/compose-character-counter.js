6fff521447f3da32be3dfcbd8161b3494b89e631
JRam2022 <jasonram778@gmail.com>
2022-04-26 17:37:11 -0700

    Add logic to update text counter and make font red if below 0

---
+++ public/scripts/compose-character-counter.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+  //const counter = document.querySelector(".counter")
+
+  $("#tweet-text").on('input', function(){
+    const textCount = $(this).val();
+    //calculates characters remaining
+    const textLeft = 140 - textCount.length
+    //colors text red if no characters remain
+    if (textLeft < 0) {
+      $(".counter").css("color", "red")
+    }
+    //updates counter
+    const textRemaining = $(".counter").text(textLeft)
+  });
+
+
+
+
+
+
+
+
+
+
+});
