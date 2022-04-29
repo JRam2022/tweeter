241e336ace02c867ad08266a51155073cb517cdd
JRam2022 <jasonram778@gmail.com>
2022-04-28 17:54:32 -0700

    Linted

--- public/scripts/compose-character-counter.js
+++ public/scripts/compose-character-counter.js
@@ -1,16 +1,15 @@
-
 $(document).ready(function(){
   $("#tweet-text").on('input', function(){
     const textCount = $(this).val();
     //calculates characters remaining
-    const textLeft = 140 - textCount.length
+    const textLeft = 140 - textCount.length;
     //colors text red if no characters remain
     if (textLeft < 0) {
-      $(".counter").css("color", "red")
+      $(".counter").css("color", "red");
     } else {
-      $(".counter").css("color", "black")
+      $(".counter").css("color", "black");
     }
-    //updates counter
-    const textRemaining = $(".counter").text(textLeft)
+    //updates tweet character counter
+    const textRemaining = $(".counter").text(textLeft);
   });
 });
